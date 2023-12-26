a7ea402b88cd7b2704287c32ff66216f614de1ca
Ninja <sleepyneko@protonmail.ch>
2023-12-26 13:46:33 +0200

    Try json database

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import seathVid from './assets/seathbomb2.webm'
 import seathThumb from './assets/seathThumb.png'
+import videosjson from './assets/videos.json'
 import styled from 'styled-components'
 import VideoCard from './VideoCard'
+import axios from 'axios'
 
 const VideoList = styled.div`
 	display: flex;
@@ -12,13 +14,39 @@ const VideoList = styled.div`
 `
 
 function App() {
+	const [videos, setVideos] = useState()
+
+	// useEffect(() => {
+	// 	axios.get('http://localhost:1337/api/videos?populate=*', {
+	// 		headers: {
+	// 			'Authorization': 'Bearer 62b870bf3236b0e17cee3ff03395c4594f8486cbdb70320953012f85c7882b76b05d9dba765c4ad33f10a0810660e970fb0c01d7b7401ecaa14ddcd8a7857a68e37058470d2768ac28798730436943acc8bb50439edbd8cfbd4968bce275af067d7c6d67613db2479bba4664e22501df474184f3ab3c318c95ddf055c95dcc61'
+	// 		}
+	// 	})
+	// 		.then((res) => { console.log(res.data.data); setVideos(res.data.data) })
+	// 		.catch((err) => console.error(err))
+
+	// }, [setVideos])
+	useEffect(() => {
+		setVideos(videosjson.videos)
+
+	}, [setVideos])
+
 
 	return (
 		<>
 			<VideoList>
-				<VideoCard video={seathVid} thumbnail={seathThumb}></VideoCard>
-				<VideoCard video={seathVid} thumbnail={seathThumb}></VideoCard>
-			</VideoList>
+			{videos && videos.map((video) => (
+				<VideoCard
+					key={video.id}
+					title={video.title}
+					video={video.video}
+					thumbnail={video.thumbnail}
+				>
+				</VideoCard >
+			))}
+			{/* <VideoCard video={seathVid} thumbnail={seathThumb}></VideoCard>
+			<VideoCard video={seathVid} thumbnail={seathThumb}></VideoCard> */}
+		</VideoList>
 		</>
 	)
 }
--- src/VideoCard.jsx
+++ src/VideoCard.jsx
@@ -1,22 +1,37 @@
 import { useState } from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const VideoContainer = styled.div`
+    position: relative;
     margin: 10px;
+    flex-direction: vertical;
+    width: 480px;
+    height: 360px;
+    overflow: hidden;
+`
+
+const Video = styled.video`
     height: 360px;
     width: 480px;
 `
 
-const Video = styled.video`
+const Title = styled.div`
+    position: absolute;
+    top: ${(props) => (props.$active ? '0px' : '-35px')};
+    color: white;
+    background-color: #0000004c;
+    padding: 5px;
+    text-align: center;
+    transition: 0.3s ease-out;
     width: 100%;
-    height: 100%;
 `
 
-const VideoCard = ({ video, thumbnail }) => {
+const VideoCard = ({ video, thumbnail, title }) => {
     const [hovered, setHovered] = useState(false)
 
     return (
         <VideoContainer>
+            <Title $active={hovered}>{title}</Title>
             <Video src={video} poster={thumbnail} controls={hovered} onMouseOver={() => setHovered(true)} onMouseLeave={() => setHovered(false)}></Video>
         </VideoContainer>
     )
