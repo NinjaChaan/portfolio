9d389d94c8c97917a4bb1acb22810e4c21466fc4
Ninja <sleepyneko@protonmail.ch>
2024-02-03 12:42:17 +0200

    Stuffen

--- src/VideoCard.jsx
+++ src/VideoCard.jsx
@@ -1,52 +1,66 @@
 import { useState } from 'react'
 import styled, { css } from 'styled-components'
-import {isMobile} from 'react-device-detect'
+import { isMobile } from 'react-device-detect'
 
 const VideoContainer = styled.div`
     position: relative;
     margin-top: 25px;
     margin-bottom: 25px;
-    margin-left: ${(props) => (props.$isMobile ? '5px' : '25px')};
-    margin-right: ${(props) => (props.$isMobile ? '5px' : '25px')};
+    margin-left: ${(props) => (props.$isMobile ? '0px' : '25px')};
+    margin-right: ${(props) => (props.$isMobile ? '0px' : '25px')};
     width: 100%;
-    height: ${(props) => (props.$isMobile ? 'auto' : '100%')};
-    min-height: ${(props) => (props.$isMobile ? '0' : '500px')};
+    height: ${(props) => (props.$isMobile ? 'fit-content' : '100%')};
+    min-height: ${(props) => (props.$isMobile ? '300px' : '500px')};
     max-height: 500px;
     background-color: #1c1c1c;
     flex: ${(props) => (props.$isMobile ? '100%' : '40%')};
     display: flex;
     justify-content: center;
-    padding-bottom: ${(props) => (props.$isMobile ? '56.25%' : '0')};
+    padding: ${(props) => (props.$isMobile ? '0px 5px 5px 5px' : '0')};
+    flex-direction: column;
     
 `
 
 const Video = styled.video`
-    position: absolute;
     height: 100%;
     width: 100%;
     max-height: 480px;
     max-width: 720px;
     margin: ${(props) => (props.$isMobile ? '0px' : '5px')};
+    padding: ${(props) => (props.$isMobile ? '10px 0px 5px 0' : '0')};
+    position: ${(props) => (props.$isMobile ? '' : 'absolute')};
+    align-self: center;
 `
 
-const Title = styled.div`
-    position: absolute;
-    top: ${(props) => (props.$active ? '-32px' : '0px')};
+const InfoText = styled.div`
+    position: ${(props) => (props.$isMobile ? 'initial' : 'absolute')};
+    top: ${(props) => (props.$active ? '-32px' : '5px')};
     transition: 0.3s ease-out;
     color: white;
     padding: 5px;
     text-align: center;
     width: 100%;
     background-color: #1c1c1c;
-    z-index: -1;
 `
 
-const VideoCard = ({ video, thumbnail, title }) => {
+const Title = styled.div`
+
+`
+
+const Description = styled.div`
+    padding-top: 10px;
+`
+
+const VideoCard = ({ video, thumbnail, title, description }) => {
     const [hovered, setHovered] = useState(false)
 
     return (
         <VideoContainer $isMobile={isMobile} onMouseOver={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
-            <Title $active={(isMobile || hovered)} >{title}</Title>
+            <InfoText $isMobile={isMobile} $active={(isMobile || hovered)}>
+                <Title>{title}</Title>
+                <Description>{description}</Description>
+                
+            </InfoText>
             <Video $isMobile={isMobile} src={video} poster={thumbnail} controls={(isMobile || hovered)} loop></Video>
         </VideoContainer>
     )
--- src/VideoList.jsx
+++ src/VideoList.jsx
@@ -2,13 +2,14 @@ import { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import videosjson from './assets/videos.json'
 import VideoCard from './VideoCard'
-import {isMobile} from 'react-device-detect'
+import { isMobile } from 'react-device-detect'
 
 const VideoListContainer = styled.div`
-	display: grid;
+	display:  ${(props) => (props.$isMobile ? 'flex' : 'grid')};
+    flex-direction: column;
     grid-row-gap: 50px;
-    grid-template-columns: ${(props) => (props.$isMobile ? '90%' : '40% 40%')};
-    justify-content: space-evenly;
+    grid-template-columns: ${(props) => (props.$isMobile ? '' : '40% 40%')};
+    justify-content: ${(props) => (props.$isMobile ? '' : 'space-evenly')};
     padding: ${(props) => (props.$isMobile ? '0px' : '25px')};
     width: 100%;
 `
@@ -17,7 +18,8 @@ const VideoList = () => {
     const [videos, setVideos] = useState()
 
     useEffect(() => {
-        setVideos(videosjson.videos)
+        var temp = [].concat(videosjson.videos).reverse()
+        setVideos(temp)
 
     }, [setVideos])
 
@@ -29,6 +31,7 @@ const VideoList = () => {
                     title={video.title}
                     video={'/portfolio/' + video.video}
                     thumbnail={'/portfolio/' + video.thumbnail}
+                    description={video.description}
                 >
                 </VideoCard >
             ))}
